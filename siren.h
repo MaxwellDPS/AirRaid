6184103e6f49c21d13b21dddcac2bcb9b1213470
Maxwell Watermolen <mwatermolen@maxwelldps.com>
2017-08-09 12:32:40 -0500

    Update siren.h

--- siren.h
+++ siren.h
@@ -13,17 +13,17 @@ COPYRIGHT MAX WATERMOLEN 2017
 # define siren
 #define CODE_LENGHT					5
 
-void  SirenStop  (int cur);																//THIS FUNCTION REVS DOWN THE SIREN AND KILLS THE PWM HOLD ON THE PIN
-void* SirenGrowl (void *c);																//THIS FUNCTION REVES THE SIREN UP TO A GROWL UNTIL CNCL = 1 THE IT CALLS SIRENSTOP()
-void* SirenAlert (void *c);																//THIS FUNCTION REVES THE SIREN UP TO ALERT UNTIL CNCL = 1 THE IT CALLS SIRENSTOP()
-void* SirenAttack(void *c);																//THIS FUNCTION REVES THE SIREN UP TO ATTACK AND WAILS UNTIL CNCL = 1 THE IT CALLS SIRENSTOP()
-void  SoundSiren (int mode, int *go);									//THIS FUNCTION CREATES A THREAD FOR THE SIREN FUNCTION THEN PASSES CNCL VAR
-void  DTMFhandel (char *dtmf, int charcount, int timestamp, int *Cancel_Var);			//HANDELS DTMF RECOGNITION
-void  DTMFcheck (char *dtmf, int *Cancel_Var);
-void  CleanChar (char *var); 
+void  SirenStop  (int cur);												//THIS FUNCTION REVS DOWN THE SIREN AND KILLS THE PWM HOLD ON THE PIN
+void* SirenGrowl (void *c);												//THIS FUNCTION REVES THE SIREN UP TO A GROWL UNTIL CNCL = 1 THE IT CALLS SIRENSTOP()
+void* SirenAlert (void *c);												//THIS FUNCTION REVES THE SIREN UP TO ALERT UNTIL CNCL = 1 THE IT CALLS SIRENSTOP()
+void* SirenAttack(void *c);												//THIS FUNCTION REVES THE SIREN UP TO ATTACK AND WAILS UNTIL CNCL = 1 THE IT CALLS SIRENSTOP()
+void  SoundSiren (int mode, int *go);				    	//THIS FUNCTION CREATES A THREAD FOR THE SIREN FUNCTION THEN PASSES CNCL VAR
+void  DTMFhandel (char *dtmf, int charcount, int timestamp, int *Cancel_Var);			//THIS FUNCTION HANDELS DTMF RECOGNITION
+void  DTMFcheck (char *dtmf, int *Cancel_Var);   //THIS FUNCTION CHECKS THE DTMF CHAR
+void  CleanChar (char *var);                     //THIS FUNCTION CLEARS THE DTMF CHAR
 
-extern char dtmf_string[CODE_LENGHT + 1]; 
-extern int last_time; 
-extern int dtmf_count;
+extern char dtmf_string[CODE_LENGHT + 1];        //DTMF CHAR
+extern int last_time;                            // TIME OF LAST RECIVED DTMF CHAR
+extern int dtmf_count;                           //COUNT OF RECIEVED DTMF CHARS
 
 #endif
